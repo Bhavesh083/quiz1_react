a6ceaf3c5601e853eca2463f311e9da34164d632
Bhavesh083 <19113083@student.hindustanuniv.ac.in>
2021-01-12 00:18:15 +0530

    Changed CSS & logic

--- src/App.js
+++ src/App.js
@@ -1,99 +1,75 @@
 import React, { useState } from 'react';
+import Quizdata from './Quizdata'
 import Ap from './Ap.css'; 
 
-function App() {
-  const [curQues, setCurQues] = useState(0) ;
-  const [score, setScore] = useState(0);
-  const [previd,setPrevid] = useState(null);
-  const [showScore, setShowScore] = useState(false);
+function App(){
+	
+const [curindex, setCurindex] = useState(0);
+const [isEnd, setIsEnd] = useState(false);
+const [score, setScore] = useState(0);
+const [userAns, setUserAns] = useState(null);
 
-const questions = [
-	   {
-		    questionText: 'Who is the founder of Linux?',
-	     	options: [
-			{answerText: 'Linus Torvalds', isCorrect: true ,id:1 },
-			{answerText: 'Mark Zuckerberg',isCorrect: false,id:2 },
-			{answerText: 'Chris Wanstrath',isCorrect: false,id:3 },
-			{answerText: 'Microsoft',      isCorrect: false,id:4 },
-       ],
-     	},
-		{
-			questionText: '23*10=',
-			options: [
-				{ answerText: '238', isCorrect: false,id:1},
-				{ answerText: '173', isCorrect: false,id:2},
-				{ answerText: '298', isCorrect: false,id:3},
-				{ answerText: '230', isCorrect: true, id:4},
-      ],
-		},
-		{
-			questionText: 'Who owns Iphone?',
-			options: [
-				{answerText: 'Apple',     isCorrect: true ,id:1 },
-				{answerText: 'Intel',     isCorrect: false,id:2 },
-				{answerText: 'Amazon',    isCorrect: false,id:3 },
-				{answerText: 'Microsoft', isCorrect: false,id:4 },
-      ],
-		},
-		{
-			questionText: '896-296',
-			options: [
-				{ answerText: '623',isCorrect:false,id:1 },
-				{ answerText: '600' ,isCorrect:true, id:2},
-				{ answerText: '500',isCorrect:false,id:3},
-				{ answerText: '596',isCorrect:false,id:4}
-      ],
-    },
-    {
-			questionText: '1000%200',
-			options: [
-				{ answerText: '12', isCorrect:false,id:1},
-				{ answerText: '8', isCorrect: false,id:2},
-				{ answerText: '5', isCorrect: true ,id:3},
-				{ answerText: '4', isCorrect: false,id:4},
-      ],
-		}
-  ];
-const checkScore = (isCorrect,id) =>{
-  if(previd !== id){
-  if(isCorrect) {
-    var scr = score+1;
-    setScore(scr);
-    setPrevid(id);
-      }}
-}  
-  const changeHandler = () =>{ 
-    setPrevid(null);
-  const newQues = curQues + 1 ;
-   if(newQues < questions.length){
-   setCurQues(newQues);
-   }else{ 
-     setShowScore(true);
-   }
+const nextQues = () => {
+
+   setUserAns(null);
+   
+   if(curindex < Quizdata.length-1){
+	  setCurindex(curindex + 1)
 }
-	return (
+   if (curindex === Quizdata.length-1) {
+	  setIsEnd( true)
+}
+   if(userAns===Quizdata[curindex].answer){
+	  setScore(score+1)
+}
+}
+
+
+const checkAns = (answers) =>{
+   setUserAns(answers)
+}
+
+
+return (
 		<div className='app'>
-			{showScore? (
-				<div className='score-section'>Your score is  {score}/{questions.length}</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>{curQues+1}</span>/{questions.length}
-						</div>
-              <div className='question-text'>
-               {questions[curQues].questionText}
-              </div>  
-					</div>
-          <div className='answer-section' >
-          {questions[curQues].options.map((option) => 
-              (<button type='button'  key={option.id} onClick={() => checkScore(option.isCorrect,option.id)}>{option.answerText}</button>
-          ))}
-          </div>
-          <button className='next' onClick={()=>changeHandler()}>{curQues<questions.length-1?'Next': 'Finish'}</button>
-				</>
-			)}
+			{isEnd? 
+			(<div className='score-selection'>
+				 <h2>You scored {score}/{Quizdata.length}</h2>
+				 {Quizdata.map(data =>
+				 <div className='keyshow'>
+					<p>
+					  {data.question}
+					</p>
+					<p>
+					 Correct answer : {data.answer}
+					</p>
+				 </div>
+				 )}
+			</div>)
+			:
+			(<>
+			<div className='question-section'>
+				<div className='question-count'>
+						<span>{curindex+1}</span>/{Quizdata.length}
+				</div>
+				<div className='question-text'>
+					{Quizdata[curindex].question}
+				</div>
+			</div>    
+			<div className='answer-section' >
+				{Quizdata[curindex].options.map(option => 
+				<button onClick={()=>checkAns(option)}>
+					{option}
+				</button>
+				 )}
+			</div>     
+			<button className='next' onClick={()=>nextQues()}>
+					{curindex<Quizdata.length-1?'Next':'Submit'}
+			</button>
+			</>
+			)} 
 		</div>
-	);
+	)
 }
-export default App ;
+
+export default App;
---
+++ src/Quizdata.js
@@ -0,0 +1,34 @@
+const Quizdata = [
+    { 
+        id :0,
+         question: 'Who is the founder of Linux?',
+         options: ['Linus Torvalds', 'Mark Zuckerberg','Chris Wanstrath','Microsoft'],
+         answer : 'Linus Torvalds'
+    },
+    {
+        id:1,
+         question: '23*10=?',
+         options: ['238','173','298','230'],
+         answer : '230'
+    },
+    {
+        id:2,
+         question: 'Who owns Iphone?',
+         options: ['Apple','Intel','Amazon','Microsoft'],
+         answer : 'Apple'
+    },
+    {
+        id:3,
+         question: '896-296=?',
+         options: ['623','600','500','596'],
+         answer : '600'
+    },
+    {
+        id:4,
+         question: '1000%200=?',
+         options: ['12','8','5','4'],
+         answer : '5'
+     }
+    ] 
+
+export default Quizdata;
